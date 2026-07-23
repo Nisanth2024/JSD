b04f0c75cfbbe2a04b135426b0b6f7c8ebc8ed62
Nisanth2024 <nisanthn322@gmail.com>
2026-07-23 13:08:09 +0530

    Theory added

--- Basic Programs/First.java
+++ Basic Programs/First.java
@@ -3,6 +3,9 @@ public class First
     public static void main(String  args[])
     {
         System.out.println("Welcome to Java");
+        System.out.println();
+        System.out.format("Welcome to Java");
+
     }
 
 }
