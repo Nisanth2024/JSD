1a4c4ccf21f2b51e184683af4f041642cda99916
Nisanth2024 <nisanthn322@gmail.com>
2026-07-23 13:24:03 +0530

    New Changes in First.java program

--- Basic Programs/First.java
+++ Basic Programs/First.java
@@ -4,7 +4,12 @@ public class First
     {
         System.out.println("Welcome to Java");
         System.out.println();
-        System.out.format("Welcome to Java");
+        System.out.format("Welcome to Java \n");
+
+        String name = "Nisanth";
+        int age = 21;
+        float height = 167.5f;
+        System.out.format("\n My name is %s.\n I am %d years old.\n my height is %.2f cm.\n", name, age, height);
 
     }
 
