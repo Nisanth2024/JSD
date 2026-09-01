c42a58ea63794c23bf57cfac124e0c908358da6d
Nisanth2024 <nisanthn322@gmail.com>
2026-09-01 11:04:15 +0530

    Day31

--- Mysql/Timestamp.sql
+++ Mysql/Timestamp.sql
@@ -12,5 +12,21 @@ use jsd;
 #truncate table test1;
 
 #create table sales(pno int primary key auto_increment, pname varchar(25),salesdate timestamp default current_timestamp);
-insert into sales(pname) values('juice');
-select * from sales
+#insert into sales(pname) values('juice');
+#select * from sales
+
+use jsd;
+#create table testmark(sno int,sname varchar(25),mark int check(mark>=0 and mark<=100));
+#insert into testmark values(1002,'Nisanth',0);
+#select * from testmark;
+
+#create table testnull(sno int,sname varchar(25) not null,mark int);
+#insert into testnull values(1112,'Nisa',90);
+#select * from testnull;
+
+#create table test3(rno int primary key,sname varchar(22), mark int);
+#create table test4(rno int primary key,sname varchar(22), mark int, foreign key(rno) references test3(rno));
+#insert into test3 values(101,'Nisanth',100),(102,'Abi',100),(103,'Nisa',100),(104,'Kavya',100);
+insert into test4 values(101,'Abinaya',90);
+select * from test4;
+
